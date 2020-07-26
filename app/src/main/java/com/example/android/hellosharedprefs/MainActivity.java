a5f3bb7292b2bc140241dee0306bb40447e53239
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-07-26 06:46:57 +0530

    Reconfiguring project with androidx dependencies

--- app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
+++ app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
@@ -16,11 +16,12 @@
 package com.example.android.hellosharedprefs;
 
 import android.graphics.drawable.ColorDrawable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 /**
  * HelloSharedPrefs is an adaptation of the HelloToast app from chapter 1.
